c5cf132e975617574b60e6b7e4ad23190963465f
NGUYENVODUCCR <nguyenvoduc2004@gmail.com>
2025-10-21 03:03:42 +0700

    fix cors commit

--- src/main/java/com/atp/fwfe/config/SecurityConfig.java
+++ src/main/java/com/atp/fwfe/config/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:*", "http://127.0.0.1:*"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:*", "http://127.0.0.1:*", "http://152.42.196.211",));
         configuration.setAllowedOrigins(List.of("http://10.0.2.2:8000", "http://127.0.0.1:8000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of(
