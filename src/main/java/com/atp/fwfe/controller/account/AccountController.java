5b504892d83a419c006882ebb398b0af537e2464
NGUYENVODUCCR <nguyenvoduc2004@gmail.com>
2025-11-03 12:30:50 +0700

    update security config CORS fix

--- src/main/java/com/atp/fwfe/controller/account/AccountController.java
+++ src/main/java/com/atp/fwfe/controller/account/AccountController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/account")
 @CrossOrigin(origins = {
-        "http://152.42.196.211:80",
+        "http://152.42.196.211",
         "http://10.0.2.2:8000",
         "http://127.0.0.1:8000",
         "http://localhost:*"
