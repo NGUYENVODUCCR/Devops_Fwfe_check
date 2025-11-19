35408f0225906851a90bf57b0e482db7dabef0e3
NGUYENVODUCCR <nguyenvoduc2004@gmail.com>
2025-11-19 21:59:26 +0700

    fix delete commit

--- src/main/java/com/atp/fwfe/model/account/Account.java
+++ src/main/java/com/atp/fwfe/model/account/Account.java
@@ -12,6 +12,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import java.util.Set;
+import java.util.HashSet;
+import com.atp.fwfe.model.chat.ChatMessage;
 
 import java.time.LocalDateTime;
 import java.util.List;
