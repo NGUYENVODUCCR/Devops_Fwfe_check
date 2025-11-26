b301b673489a6d01ebe566a0bc669042d8886807
NGUYENVODUCCR <nguyenvoduc2004@gmail.com>
2025-11-26 21:19:17 +0700

    fix mailer

--- src/main/java/com/atp/fwfe/config/MailConfig.java
+++ src/main/java/com/atp/fwfe/config/MailConfig.java
@@ -1,6 +1,7 @@
 package com.atp.fwfe.config;
 
 import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,8 +11,27 @@ import java.util.Properties;
 
 @Configuration
 public class MailConfig {
- @Bean
-    public JavaMailSender javaMailSender() {
-        return new JavaMailSenderImpl();
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() throws MessagingException{
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
     }
 }
